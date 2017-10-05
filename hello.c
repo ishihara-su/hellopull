4374ba3600aff177baa1c57e81fd1a8d2ccb78fd
ishihara-su <ishihara.susumu@shizuoka.ac.jp>
2017-10-05 14:05:44 +0900

    Added hey, hey, hey.

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /* hello.c - Susumu Ishihara */
 #include <stdio.h>
 int main() {
-    puts("Woo yeah! Good morning world!");
+    puts("Hey, hey, hey, Woo yeah! Good morning world!");
     return 0;
 }
